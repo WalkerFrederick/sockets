5daacd68a232b070a63dca2f4ef4ba2815ac1fb8
“wfrederick” <iosdev@pnfMBP.attlocal.net>
2021-06-08 12:08:59 -0400

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('<h1>index</h1>');
+});
+
+app.get('/requestPickup', (req, res) => {
+    
+    io.sockets.emit('pickupRequest', req.query.lotCode, "A user has requested pickup");
+
+    res.status(200).send('success')
+});
+
+app.get('/pickup-notif', (req, res) => {
+    res.send('<h1>index</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('location', (shuttleName, lotCdoe, newPos) => {
+      console.log('NEW POS', shuttleName);
+    });
+    socket.on("pickupRequest", (arg1, arg2) => {
+        console.log(arg1, arg2)
+    });
+});
+
+
+
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
